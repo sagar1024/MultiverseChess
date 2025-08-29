69c08f9bbb0f2c3027831b8db5595c7f2100029a
sagar <sagargurung272@gmail.com>
2025-08-29 22:23:21 +0530

    Improved readability

--- frontend/multiverse-chess/src/App.tsx
+++ frontend/multiverse-chess/src/App.tsx
@@ -1,27 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router } from "react-router-dom";
-// import Navbar from "./components/layout/Navbar";
-// import Footer from "./components/layout/Footer";
-// import PageWrapper from "./components/layout/PageWrapper";
-// import AppRouter from "./router";
-
-// const App: React.FC = () => {
-//   return (
-//     <Router>
-//       <div className="flex flex-col min-h-screen bg-[#1e1e2f] text-white">
-//         <Navbar />
-//         <PageWrapper>
-//           <AppRouter />
-//         </PageWrapper>
-
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
