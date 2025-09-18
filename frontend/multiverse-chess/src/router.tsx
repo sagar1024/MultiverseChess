d6c2305163273eb432e9912e6209dde7a00538c1
sagar <sagargurung272@gmail.com>
2025-09-18 23:33:57 +0530

    Added JoinGame route

--- frontend/multiverse-chess/src/router.tsx
+++ frontend/multiverse-chess/src/router.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import JoinGame from "./pages/JoinGame";
 import GameRoom from "./pages/GameRoom";
 import GameOver from "./pages/GameOver";
 import NotFoundPage from "./pages/NotFound";
@@ -9,6 +10,7 @@ const AppRouter: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/join" element={<JoinGame />} />
       <Route path="/game/:gameId" element={<GameRoom />} />
       <Route path="/game/:gameId/over" element={<GameOver />} />
       <Route path="*" element={<NotFoundPage />} />
