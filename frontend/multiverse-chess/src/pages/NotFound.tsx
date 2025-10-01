5733ec3593eb86620d20ba766b77e84c88639b97
sagar <sagargurung272@gmail.com>
2025-10-01 23:24:04 +0530

    Some changes

--- frontend/multiverse-chess/src/pages/NotFound.tsx
+++ frontend/multiverse-chess/src/pages/NotFound.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const NotFound: React.FC = () => {
     const navigate = useNavigate();
+    
     return (
         <div className="h-screen flex flex-col justify-center items-center bg-gray-950 text-white">
             <h1 className="text-3xl font-bold mb-4">Game Not Found</h1>
