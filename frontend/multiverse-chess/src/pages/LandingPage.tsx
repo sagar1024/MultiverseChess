2a26f2e253f1cc05216322c987289f31847c83b4
sagar <sagargurung272@gmail.com>
2025-10-01 23:24:59 +0530

    Some minor changes

--- frontend/multiverse-chess/src/pages/LandingPage.tsx
+++ frontend/multiverse-chess/src/pages/LandingPage.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { createGame } from "../utils/storage";
-import { v4 as uuidv4 } from "uuid";
 import IntroSection from "../components/landing/IntroSection";
 import RulesSection from "../components/landing/RulesSection";
 import CTAButtons from "../components/landing/CTAButtons";
 import PageWrapper from "../components/layout/PageWrapper";
+
+import { createGame } from "../utils/storage";
+import { v4 as uuidv4 } from "uuid";
 
 const LandingPage: React.FC = () => {
     const navigate = useNavigate();
