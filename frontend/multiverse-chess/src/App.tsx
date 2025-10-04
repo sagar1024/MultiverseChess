2cfbbd6fbc235eabbb7cddcc5fe3183d088e0203
sagar <sagargurung272@gmail.com>
2025-10-04 23:35:55 +0530

    App.tsx changed

--- frontend/multiverse-chess/src/App.tsx
+++ frontend/multiverse-chess/src/App.tsx
@@ -1,3 +1,31 @@
+// import React from "react";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import Navbar from "./components/layout/Navbar";
+// import Footer from "./components/layout/Footer";
+// import PageWrapper from "./components/layout/PageWrapper";
+// import AppRouter from "./router";
+
+// const App: React.FC = () => {
+//   return (
+//     <Router>
+//       <div className="flex flex-col min-h-screen bg-[#1e1e2f] text-white">
+//         <Navbar />
+
+//         {/* Keep consistent layout & animations for all routes */}
+//         <main className="flex-1">
+//           <PageWrapper>
+//             <AppRouter />
+//           </PageWrapper>
+//         </main>
+
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
@@ -9,15 +37,15 @@ const App: React.FC = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-[#1e1e2f] text-white">
+        {/* Global Navbar */}
         <Navbar />
 
-        {/* Keep consistent layout & animations for all routes */}
-        <main className="flex-1">
-          <PageWrapper>
-            <AppRouter />
-          </PageWrapper>
-        </main>
+        {/* Page content wrapper */}
+        <PageWrapper>
+          <AppRouter />
+        </PageWrapper>
 
+        {/* Global Footer */}
         <Footer />
       </div>
     </Router>
