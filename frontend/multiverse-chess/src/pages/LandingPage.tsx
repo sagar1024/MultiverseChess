e98aeba6b5e2d8e5552f5726c811b4c50e7e9aec
sagar <sagargurung272@gmail.com>
2025-10-04 23:37:22 +0530

    Improved Readability

--- frontend/multiverse-chess/src/pages/LandingPage.tsx
+++ frontend/multiverse-chess/src/pages/LandingPage.tsx
@@ -1,38 +1,3 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
-// import IntroSection from "../components/landing/IntroSection";
-// import RulesSection from "../components/landing/RulesSection";
-// import CTAButtons from "../components/landing/CTAButtons";
-// import PageWrapper from "../components/layout/PageWrapper";
-
-// import { createGame } from "../utils/storage";
-// import { v4 as uuidv4 } from "uuid";
-
-// const LandingPage: React.FC = () => {
-//     const navigate = useNavigate();
-
-//     const handleCreateGame = () => {
-//         const newId = uuidv4();
-//         createGame(newId, "Host");
-//         navigate(`/game/${newId}`);
-//     };
-
-//     const handleJoinGame = () => {
-//         //Go to a "Join Game" page where user can paste a link or code
-//         navigate("/join");
-//     };
-
-//     return (
-//         <PageWrapper>
-//             <IntroSection />
-//             <RulesSection />
-//             <CTAButtons onCreateGame={handleCreateGame} onJoinGame={handleJoinGame} />
-//         </PageWrapper>
-//     );
-// };
-
-// export default LandingPage;
-
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
