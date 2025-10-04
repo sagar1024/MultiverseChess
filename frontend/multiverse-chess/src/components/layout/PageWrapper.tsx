6af8c2230ccdf371fb0532cb810e3eb5c06daf35
sagar <sagargurung272@gmail.com>
2025-10-04 23:34:22 +0530

    Updated PageWrapper

--- frontend/multiverse-chess/src/components/layout/PageWrapper.tsx
+++ frontend/multiverse-chess/src/components/layout/PageWrapper.tsx
@@ -1,28 +1,45 @@
+// import React from "react";
+// import Navbar from "./Navbar";
+// import Footer from "./Footer";
+
+// interface PageWrapperProps {
+//     children: React.ReactNode;
+//     onCreateGame?: () => void;
+//     onJoinGame?: () => void;
+// }
+
+// const PageWrapper: React.FC<PageWrapperProps> = ({
+//     children,
+//     onCreateGame,
+//     onJoinGame
+// }) => {
+//     return (
+//         <div className="flex flex-col min-h-screen bg-gray-950 text-white">
+//             {/* Navbar */}
+//             <Navbar onCreateGame={onCreateGame} onJoinGame={onJoinGame} />
+
+//             {/* Page Content */}
+//             <main className="flex-1 p-6">{children}</main>
+
+//             {/* Footer */}
+//             <Footer />
+//         </div>
+//     );
+// };
+
+// export default PageWrapper;
+
 import React from "react";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
 
 interface PageWrapperProps {
     children: React.ReactNode;
-    onCreateGame?: () => void;
-    onJoinGame?: () => void;
+    className?: string;
 }
 
-const PageWrapper: React.FC<PageWrapperProps> = ({
-    children,
-    onCreateGame,
-    onJoinGame
-}) => {
+const PageWrapper: React.FC<PageWrapperProps> = ({ children, className = "" }) => {
     return (
-        <div className="flex flex-col min-h-screen bg-gray-950 text-white">
-            {/* Navbar */}
-            <Navbar onCreateGame={onCreateGame} onJoinGame={onJoinGame} />
-
-            {/* Page Content */}
-            <main className="flex-1 p-6">{children}</main>
-
-            {/* Footer */}
-            <Footer />
+        <div className={`flex-1 flex flex-col ${className}`}>
+            <main className="flex-1 p-6 max-w-6xl w-full mx-auto">{children}</main>
         </div>
     );
 };
