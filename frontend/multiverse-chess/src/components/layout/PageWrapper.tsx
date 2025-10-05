c5262c71934c2f46b33f6a02c3ec7edd0ef5627a
sagar <sagargurung272@gmail.com>
2025-10-06 00:57:50 +0530

    Some changes

--- frontend/multiverse-chess/src/components/layout/PageWrapper.tsx
+++ frontend/multiverse-chess/src/components/layout/PageWrapper.tsx
@@ -1,34 +1,3 @@
-// import React from "react";
-// import Navbar from "./Navbar";
-// import Footer from "./Footer";
-
-// interface PageWrapperProps {
-//     children: React.ReactNode;
-//     onCreateGame?: () => void;
-//     onJoinGame?: () => void;
-// }
-
-// const PageWrapper: React.FC<PageWrapperProps> = ({
-//     children,
-//     onCreateGame,
-//     onJoinGame
-// }) => {
-//     return (
-//         <div className="flex flex-col min-h-screen bg-gray-950 text-white">
-//             {/* Navbar */}
-//             <Navbar onCreateGame={onCreateGame} onJoinGame={onJoinGame} />
-
-//             {/* Page Content */}
-//             <main className="flex-1 p-6">{children}</main>
-
-//             {/* Footer */}
-//             <Footer />
-//         </div>
-//     );
-// };
-
-// export default PageWrapper;
-
 import React from "react";
 
 interface PageWrapperProps {
