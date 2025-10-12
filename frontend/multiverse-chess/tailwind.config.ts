ee16fdceb37d798124d76e194c1152484cee1a01
sagar <sagargurung272@gmail.com>
2025-10-13 00:38:40 +0530

    Fixed bugs

--- frontend/multiverse-chess/tailwind.config.ts
+++ frontend/multiverse-chess/tailwind.config.ts
@@ -1,12 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
 import type { Config } from "tailwindcss";
 
 export default {
