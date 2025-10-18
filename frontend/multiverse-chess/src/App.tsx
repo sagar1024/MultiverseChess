8faadc35e85bba78042c972c846c49bc92bcaece
sagar <sagargurung272@gmail.com>
2025-10-18 14:49:47 +0530

    Removed commented code

--- frontend/multiverse-chess/src/App.tsx
+++ frontend/multiverse-chess/src/App.tsx
@@ -1,28 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router } from "react-router-dom";
-// import Navbar from "./components/layout/Navbar";
-// import Footer from "./components/layout/Footer";
-// import PageWrapper from "./components/layout/PageWrapper";
-// import AppRouter from "./router";
-
-// const App: React.FC = () => {
-//   return (
-//     <Router>
-//       <div className="flex flex-col min-h-screen bg-[#1e1e2f] text-white">
-//         <Navbar />
-//         <main className="flex-1">
-//           <PageWrapper>
-//             <AppRouter />
-//           </PageWrapper>
-//         </main>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
--- frontend/multiverse-chess/src/components/layout/PageWrapper.tsx
+++ frontend/multiverse-chess/src/components/layout/PageWrapper.tsx
@@ -1,20 +1,3 @@
-// import React from "react";
-
-// interface PageWrapperProps {
-//     children: React.ReactNode;
-//     className?: string;
-// }
-
-// const PageWrapper: React.FC<PageWrapperProps> = ({ children, className = "" }) => {
-//     return (
-//         <div className={`flex-1 flex flex-col ${className}`}>
-//             <main className="flex-1 p-6 max-w-6xl w-full mx-auto">{children}</main>
-//         </div>
-//     );
-// };
-
-// export default PageWrapper;
-
 import React from "react";
 
 interface PageWrapperProps {
