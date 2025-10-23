fbecf489b5eb6be45b1e5ad41b8309091ad54958
sagar <sagargurung272@gmail.com>
2025-10-23 16:43:27 +0530

    Some fixes

--- frontend/multiverse-chess/src/components/common/Loader.tsx
+++ frontend/multiverse-chess/src/components/common/Loader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 interface LoaderProps {
-    size?: number; // size in pixels
+    size?: number; //Size in pixels
 }
 
 const Loader: React.FC<LoaderProps> = ({ size = 40 }) => {
