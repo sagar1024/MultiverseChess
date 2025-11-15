ba45a2ff77d1546ec6617f7116d50d76ca32ef0d
sagar <sagargurung272@gmail.com>
2025-11-15 21:44:23 +0530

    Resturcturing

--- frontend/multiverse-chess/src/components/game/ActiveTurnIndicator.tsx
+++ frontend/multiverse-chess/src/components/game/ActiveTurnIndicator.tsx
@@ -6,10 +6,8 @@ interface ActiveTurnIndicatorProps {
 
 const ActiveTurnIndicator: React.FC<ActiveTurnIndicatorProps> = ({ isActive }) => {
     return (
-        <div
-            className={`w-3 h-3 rounded-full ${isActive ? "bg-green-400 animate-pulse" : "bg-gray-500"
-                }`}
-        ></div>
+        <div className={`w-3 h-3 rounded-full ${isActive ? "bg-green-400 animate-pulse" : "bg-gray-500"}`}>
+        </div>
     );
 };
 
